8a7750e806bbb2c191eb23acc3b52ee6a150a440
ekeamadi <danzzeloil.gas@yahoo.com>
2023-11-23 17:08:51 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+"# Employee Service API"
+
+#### Swagger url
+```bash
+http://localhost:9090/employee-service/swagger-ui.html
+```
---
+++ src/src/main/java/za/co/CrudApi/controller/EmployeeController.java
@@ -0,0 +1,54 @@
+package za.co.CrudApi.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import za.co.CrudApi.entity.Employee;
+import za.co.CrudApi.exception.EmployeeNotFoundException;
+import za.co.CrudApi.service.EmployeeService;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+    private Logger logger = LoggerFactory.getLogger( EmployeeController.class );
+
+
+    @PostMapping("/saveEmployeeDetails")
+    @CrossOrigin
+    @ResponseBody
+    public ResponseEntity<Employee> saveEmployeeDetails(@Valid @RequestBody Employee employee) {
+        Employee _employee = employeeService.saveEmployeeDetails( employee );
+        return new ResponseEntity<>( _employee, HttpStatus.CREATED );
+    }
+
+
+    @PutMapping("/updateEmployeeDetails")
+    @CrossOrigin
+    @ResponseBody
+    public ResponseEntity<Employee> updateEmployeeDetails(@Valid @RequestBody Employee employee) {
+        Employee _employee = employeeService.updateEmployeeDetails( employee );
+        return new ResponseEntity<>( _employee, HttpStatus.CREATED );
+
+    }
+
+
+    @CrossOrigin
+    @GetMapping("/searchByFirstNameOrIdNumberOrAccountNumber/{searchName}")
+    public ResponseEntity<Employee> searchByFirstNameOrIdNumberOrAccountNumber(@PathVariable("searchName") String firstName, @PathVariable("searchName")
+    String idNumber, @PathVariable("searchName") String mobileNumber) {
+        Employee employee = employeeService.searchByFirstNameOrIdNumberOrBankAccountNumber( firstName, idNumber, mobileNumber )
+                .orElseThrow( () -> new EmployeeNotFoundException( String.format(
+                        "Employee not found for: %1$s ", firstName != null ? firstName : idNumber != null ? idNumber : mobileNumber != null ? mobileNumber : " " ) ) );
+        return new ResponseEntity<>( employee, HttpStatus.OK );
+    }
+
+
+}
